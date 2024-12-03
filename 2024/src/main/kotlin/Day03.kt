e8349cade8ce2080c2911333ae8117fed8cada2e
Ruben Holen <snurre@gmail.com>
2024-12-03 13:58:10 +0000

    2024 Day 03: Iterating

--- 2024/src/main/kotlin/Day03.kt
+++ 2024/src/main/kotlin/Day03.kt
@@ -1,7 +1,18 @@
 fun main() {
     val memory = readInput(3) { it }.joinToString("")
-    val r = """(mul\((\d{1,3}),(\d{1,3})\))""".toRegex()
-    fun Sequence<MatchResult>.sumProducts() = sumOf { it[2].toInt() * it[3].toInt() }
+    val r = """(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))""".toRegex()
+    fun Sequence<MatchResult>.sumProducts() = filterNot { it[2].isBlank() }.sumOf { it[2].toInt() * it[3].toInt() }
     part1 { r.findAll(memory).sumProducts() }
-    part2 { r.findAll(memory.split("do()").map { it.split("don't()")[0] }.joinToString("")).sumProducts() }
+    part2 {
+        var accept = true
+        var sum = 0
+        r.findAll(memory).forEach {
+            when (it[1]) {
+                "do()" -> accept = true
+                "don't()" -> accept = false
+                else -> if (accept) sum += it[2].toInt() * it[3].toInt()
+            }
+        }
+        sum
+    }
 }
